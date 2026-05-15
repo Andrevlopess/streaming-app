c73a5e4c4e9e6738d1a022edea87542c9bf27a6c
andrevlopes <andrellopes021@gmail.com>
2026-05-15 13:36:14 -0300

    add: frontend vite app project

---
+++ app/src/App.tsx
@@ -0,0 +1,12 @@
+
+export function App() {
+  return (
+    <div className="flex min-h-svh p-6">
+      <div className="flex min-w-0 w-full flex-col gap-4 text-sm leading-loose border border-white justify-center align-center">
+      hello
+      </div>
+    </div>
+  )
+}
+
+export default App
